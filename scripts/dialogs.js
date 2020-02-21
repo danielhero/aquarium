88df0e9901cce212bb5aeaface03f598f8e617b8
danielhero <danielhero@outlook.com>
2020-02-21 13:05:29 -0600

    added buttons w/ details to all fish

--- scripts/dialogs.js
+++ scripts/dialogs.js
@@ -17,4 +17,40 @@ const initializeDetailButtonEvents = () => {
 
     // You will be writing code below this line
 
+    // Show Bart's details when the button is clicked
+    document.querySelector("#button--bart").addEventListener(
+        "click",
+        theClickEvent => {
+            const theDialog = document.querySelector("#details--bart")
+            theDialog.showModal()
+        }
+    )
+
+    document.querySelector("#button--betty").addEventListener(
+        "click",
+        theClickEvent => {
+            const theDialog = document.querySelector("#details--betty")
+            theDialog.showModal()
+        }
+    )
+
+    document.querySelector("#button--nemo").addEventListener(
+        "click",
+        theClickEvent => {
+            const theDialog = document.querySelector("#details--nemo")
+            theDialog.showModal()
+        }
+    )
+
+    document.querySelector("#button--dory").addEventListener(
+        "click",
+        theClickEvent => {
+            const theDialog = document.querySelector("#details--dory")
+            theDialog.showModal()
+        }
+    )
+
 }
+
+export default initializeDetailButtonEvents;
+
