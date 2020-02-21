3b14be1349b88391f252ab589ac79efccba65119
danielhero <danielhero@outlook.com>
2020-02-21 10:44:25 -0600

    imported styles and created dialogs.js

---
+++ scripts/dialogs.js
@@ -0,0 +1,20 @@
+
+const initializeDetailButtonEvents = () => {
+    // CAN'T TOUCH THIS - START
+    const allCloseButtons = document.querySelectorAll(".button--close")
+
+    for (const btn of allCloseButtons) {
+        btn.addEventListener(
+            "click",
+            theEvent => {
+                const dialogElement = theEvent.target.parentNode
+                dialogElement.close()
+            }
+        )
+    }
+    // CAN'T TOUCH THIS - END
+
+
+    // You will be writing code below this line
+
+}
